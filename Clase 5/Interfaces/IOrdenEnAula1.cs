a4908dbfb301ee74ed6b86b4b5609036ba23f072
Aaron Fuenzalida <afuenzalida243@gmail.com>
2026-05-08 20:41:53 -0300

    up to excerise 4

---
+++ Clase 5/Interfaces/IOrdenEnAula1.cs
@@ -0,0 +1,7 @@
+namespace Metodologias_de_Programacion.Clase_5_Proxy__Command.Interfaces
+{
+    public interface IOrdenEnAula1
+    {
+        void ejecutar();
+    }
+}
---
+++ Clase 5/Models/Aula.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics.Contracts;
+using System.Runtime;
+using Metodologias_de_Programacion.Clase_5_Proxy__Command;
+
+namespace Metodologias_de_Programacion.Clase_5_Proxy__Command.Models
+{
+    public class Aula
+    {
+        private Teacher teacher;
+
+        public void comenzar()
+        {
+            Console.WriteLine("Comienza la clase");
+            this.teacher = new Teacher();
+        }
+
+        public void nuevoAlumno(IAlumno alumno)
+        {
+            Student alumnoAdaptado = new AlumnoAdapter(alumno);
+            this.teacher.goToClass(alumnoAdaptado);
+        }
+
+        public void claseLista()
+        {
+            this.teacher.teachingAClass();
+        }
+        
+    }
+}
