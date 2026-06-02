fd2fba17f42f1d56b9e1fedba1285a4da0eca651
Aaron Fuenzalida <afuenzalida243@gmail.com>
2026-06-02 18:43:38 -0300

    add up to exercise 2

--- Clase 6/Program.cs
+++ Clase 6/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Metodologias_de_Programacion.Clase_6_Composite__TemplateMethod.Adapter;
+using Metodologias_de_Programacion.Clase_6_Composite__TemplateMethod.Factory;
+using Metodologias_de_Programacion.Clase_6_Composite__TemplateMethod.Interfaces;
+using Metodologias_de_Programacion.Clase_6_Composite__TemplateMethod.Composite;
+
+namespace Metodologias_de_Programacion.Clase_6_Composite__TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testAdapter();
+        }
+
+       public static void testAdapter()
+        {
+            Teacher teacher = new Teacher();
+            AlumnoCompuesto alumnoCompuesto = new AlumnoCompuesto();
+    
+            for (int i = 0 ; i < 5; i++)
+            {
+                Comparable alumno = FabricaDeComparables.CrearAleatorio("Alumno");
+                alumnoCompuesto.agregarAlumno((IAlumno)alumno);
+            }
+    
+            Student alumnoAdapter = new AlumnoAdapter(alumnoCompuesto);
+    
+            teacher.goToClass(alumnoAdapter);
+    
+            teacher.teachingAClass();
+        }
+    }
+}
